f5970cb8bf2f28daa971e2b02e5d76af472482c2
maruf <70136133+abdullahamruf@users.noreply.github.com>
2021-09-11 00:17:45 +0600

    new

--- src/maximumsubarray.java
+++ src/maximumsubarray.java
@@ -5,11 +5,11 @@ public class maximumsubarray {
 	        
 	        int result = nums[0];
 	        
-	       // for(int i = 1; i < nums.length ; i ++){
-	           // nums[i] = Math.max((nums[i] + nums[i-1]), nums[i]);
-	           // if(result < nums[i])
-	               // result = nums[i];
-	       // }
+	       for(int i = 1; i < nums.length ; i ++){
+	            nums[i] = Math.max((nums[i] + nums[i-1]), nums[i]);
+	          if(result < nums[i])
+	                result = nums[i];
+	        }
 	        System.out.println(result);
 	        return result;
 	    }
